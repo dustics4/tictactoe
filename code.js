4e40dfa8987ce7c07c3972e1d0a068e5389cae33
Konrad <konrad.babowicz@gmail.com>
2023-11-22 20:53:50 +0000

    created a private function, added obj board

---
+++ code.js
@@ -0,0 +1,15 @@
+//We are going to store the gameboard inside of an array
+//The gameboard array has to be stored inside of a board Object
+//We will want players to be stored in objects too
+// Object create for Player1,player2 , symbol
+//Need to create startGame Object
+
+(function() {
+    
+    const board = {
+        gameboard : ["", "", "","","","","","",""],
+        display: this.display
+    };
+
+
+})();
