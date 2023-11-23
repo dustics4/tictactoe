2ed339cf22abbffc7317d7b811c39eaa20a53d7a
Konrad <konrad.babowicz@gmail.com>
2023-11-23 20:45:08 +0000

    displayed gameboard

--- code.js
+++ code.js
@@ -6,10 +6,31 @@
 
 (function() {
     
-    const board = {
-        gameboard : ["", "", "","","","","","",""],
-        display: this.gameboard
-    };
+    //create an array to hold the gameboard
+    const gameboard = [
+        ["-", "-", "-"],
+        ["-", "-", "-"],
+        ["-", "-", "-"]
+    ]
+    //starting with player X
+    //Making sure that gameOver is set to false, can use it as Bool in if statements later on
+    let currentPlay = "X";
+    let gameOver = false;
 
+    function displayBoard() {
+        //foreveryrow
+        for(let i = 0; i < 3; i++){
+            //make sure that row is empty
+            let row = "";
+            //foreverycolumn
+            for(let j = 0 ; j < 3; j++){
+                row += gameboard[i][j] + "";
+            }
+            console.log(row);
+        }
+    }
+
+
+    
 
 })();
